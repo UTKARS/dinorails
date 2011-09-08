5d8f92f5d83cb68b90838cf4375f06a46bf62754
Scott Tadman <github@tadman.ca>
2011-09-08 12:35:10 -0400

    Initial commit of base Rails app

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+
+gem 'haml'
+
+group :assets do
+  gem 'sass-rails', "~>3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Any requirements for the test environment
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinorails::Application.config.secret_token = 'eaed90e0aa9b0215f9b4cb9fbc635809fc58c41a4a67f4b9db3d2a827b5dcd7bd46b0d1b83ac02164b656a8e20cd4397e8b8e3d65d4d8f21097f9e8e3cc8889e'
