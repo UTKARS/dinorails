e527b8058bb323500dcc10e57521cc2415aea943
Dessy Daskalov <dessy.daskalov@gmail.com>
2011-09-12 10:16:24 -0400

    Refactor

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,7 +1,10 @@
 class Dinosaur < ActiveRecord::Base
   
+  # -- Constants ------------------------------------------------------------
+  COLOURS = %w(red blue green magenta)
+  
   # -- Validations ----------------------------------------------------------
   validates_presence_of :name
-  validates :colour, :inclusion => %w(red blue green magenta)
+  validates :colour, :inclusion => COLOURS
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+  # -- Constants ------------------------------------------------------------
   PREFIX = 'Dinosaur Owner'
 
   # -- Validations ----------------------------------------------------------
